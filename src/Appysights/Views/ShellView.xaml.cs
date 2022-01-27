44fd788db8d3334ae5ace340cd9b83b3a9cc32e9
C1rdec <cedric_lampron@hotmail.com>
2022-01-27 09:59:03 -0500

    Opt-in Windows 11 round corner

--- src/Appysights/Views/ShellView.xaml.cs
+++ src/Appysights/Views/ShellView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using MahApps.Metro.Controls;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using MahApps.Metro.Controls;
 
 namespace Appysights.Views
 {
@@ -10,6 +13,11 @@ namespace Appysights.Views
         public ShellView()
         {
             InitializeComponent();
+
+            IntPtr hWnd = new WindowInteropHelper(GetWindow(this)).EnsureHandle();
+            var attribute = DWMWINDOWATTRIBUTE.DWMWA_WINDOW_CORNER_PREFERENCE;
+            var preference = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND;
+            DwmSetWindowAttribute(hWnd, attribute, ref preference, sizeof(uint));
         }
 
         public void SetFlyoutWidth(Position position)
@@ -26,5 +34,27 @@ namespace Appysights.Views
                 MyFlyout.Width += 6;
             }
         }
+
+        public enum DWMWINDOWATTRIBUTE
+        {
+            DWMWA_WINDOW_CORNER_PREFERENCE = 33
+        }
+
+        // The DWM_WINDOW_CORNER_PREFERENCE enum for DwmSetWindowAttribute's third parameter, which tells the function
+        // what value of the enum to set.
+        public enum DWM_WINDOW_CORNER_PREFERENCE
+        {
+            DWMWCP_DEFAULT = 0,
+            DWMWCP_DONOTROUND = 1,
+            DWMWCP_ROUND = 2,
+            DWMWCP_ROUNDSMALL = 3
+        }
+
+        // Import dwmapi.dll and define DwmSetWindowAttribute in C# corresponding to the native function.
+        [DllImport("dwmapi.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern long DwmSetWindowAttribute(IntPtr hwnd,
+                                                         DWMWINDOWATTRIBUTE attribute,
+                                                         ref DWM_WINDOW_CORNER_PREFERENCE pvAttribute,
+                                                         uint cbAttribute);
     }
 }
