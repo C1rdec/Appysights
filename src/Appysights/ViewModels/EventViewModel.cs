25ece648b21a4f3db49d62ecce034fe5a1de1e81
C1rdec <cedric_lampron@hotmail.com>
2022-01-28 19:23:24 -0500

    Add the ability to copy the Action Name

--- src/Appysights/ViewModels/EventViewModel.cs
+++ src/Appysights/ViewModels/EventViewModel.cs
@@ -1,12 +1,37 @@
-﻿using Appysights.Models;
+﻿using System.Threading.Tasks;
+using Appysights.Helper;
+using Appysights.Models;
 
 namespace Appysights.ViewModels
 {
     public class EventViewModel : EventViewModelBase
     {
+        private bool _isCopiedOpen;
+
         public EventViewModel(AppInsightEvent appEvent) 
             : base(appEvent)
         {
+        }
+
+        public bool IsCopiedOpen
+        {
+            get
+            {
+                return _isCopiedOpen;
+            }
+
+            set
+            {
+                _isCopiedOpen = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public void OnActionClick()
+        {
+            IsCopiedOpen = true;
+            Task.Delay(1200).ContinueWith(t => IsCopiedOpen = false);
+            ClipboardHelper.SetText(ActionName);
         }
     }
 }
